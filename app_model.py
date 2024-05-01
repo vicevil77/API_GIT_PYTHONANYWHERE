dfa8e8f9fe683d8e8aa949f0bdc3775d6f805858
OKy9413 <jcrubgil@gmail.com>
2024-05-01 12:33:58 +0200

    up

--- app_model.py
+++ app_model.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 import os
-import pickle
+import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso
@@ -8,40 +8,47 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import numpy as np
 import subprocess
 
-root_path= "/home/JBL/taller_despliegue/"
+root_path= "/home/JBLapiweb/bike_predictor/"
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET'])
 def hello(): # Ligado al endopoint "/" o sea el home, con el método GET
-    return  "<h1>Hello, World! This is a simple API for a ML model</h1>"
+    return  "<h1>Hello! Here is you solution to predict public bike facility use in your city</h1>"
 
 @app.route('/api/v1/predict', methods= ['GET'])
 def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
 
-    model = pickle.load(open(root_path +'ad_model.pkl','rb'))
-    tv = request.args.get('tv', None)
-    radio = request.args.get('radio', None)
-    newspaper = request.args.get('newspaper', None)
+    model = joplib.load(open(root_path +'bike_model.joblib','rb'))
+    holiday = request.args.get('holiday', 0)
+    workingday = request.args.get('workingday', 1)
+    weather = request.args.get('weather', 0)
+    temp = request.args.get('temp', 0.5)
+    humidity = request.args.get('humidity', 0.65)
+    hour = request.args.get('hour', None)
+    month = request.args.get('month', None)
+    weekday = request.args.get('weekday', None)
+    windspeed_log = request.args.get('windspeed', 0)        ### PREGUNTAR CÓMO HACER MODIFICACIONES A FEATURES
 
-    print(tv,radio,newspaper)
+    features = [holiday, workingday, weather, temp, humidity, hour, month, weekday, windspeed_log]
+
+    print(tv,radio,newspaper)   # Buscar cómo hacer el print de las features !!!!
     print(type(tv))
 
-    if tv is None or radio is None or newspaper is None:
-        return "Args empty, the data are not enough to predict"
+    if hour is None or month is None or weekday is None:
+        return "Args empty, the data are not enough to predict. \n Please specify at least hour, month and weekday"
     else:
-        prediction = model.predict([[float(tv),float(radio),float(newspaper)]])
-    
+        prediction = model.predict([[float(feature) for feature in features]])     
     return jsonify({'predictions': prediction[0]})
 
 @app.route('/api/v1/retrain/', methods=['GET'])
 def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
-    if os.path.exists(root_path +"data/Advertising_new.csv"):
-        data = pd.read_csv(root_path +'data/Advertising_new.csv')
+    if os.path.exists(root_path +"data/bike_train.csv"):
+        data = pd.read_csv(root_path +'data/bike_train.csv')
 
-        X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
-                                                        data['sales'],
+        X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['count']),
+                                                        data['count'],
                                                         test_size = 0.20,
                                                         random_state=42)
 
@@ -49,8 +56,8 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
         model.fit(X_train, y_train)
         rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
         mape = mean_absolute_percentage_error(y_test, model.predict(X_test))
-        model.fit(data.drop(columns=['sales']), data['sales'])
-        pickle.dump(model, open(root_path +'ad_model.pkl', 'wb'))
+        model.fit(data.drop(columns=['count']), data['count'])
+        joblib.dump(model, open(root_path +'bike_model.joblib', 'wb'))
 
         return f"Model retrained. New evaluation metric RMSE: {str(rmse)}, MAPE: {str(mape)}"
     else:
@@ -59,8 +66,8 @@ def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
 @app.route('/webhook_2024', methods=['POST'])
 def webhook():
     # Ruta al repositorio donde se realizará el pull
-    path_repo = "/home/vicevil/taller_despliegue"
-    servidor_web = '/var/www/vicevil_pythonanywhere_com_wsgi.py' 
+    path_repo = root_path
+    servidor_web = '/var/www/jblapiweb_pythonanywhere_com_wsgi.py' 
 
     # Comprueba si la solicitud POST contiene datos JSON
     if request.is_json:
@@ -75,19 +82,19 @@ def webhook():
             try:
                 os.chdir(path_repo)
             except FileNotFoundError:
-                return jsonify({'message': 'El directorio del repositorio no existe'}), 404
+                return jsonify({'message': "This directory doesn't exist"}), 404
 
             # Realiza un git pull en el repositorio
             try:
                 subprocess.run(['git', 'pull'], check=True)
                 subprocess.run(['touch', servidor_web], check=True) # Trick to automatically reload PythonAnywhere WebServer
-                return jsonify({'message': f'Se realizó un git pull en el repositorio {repo_name}'}), 200
+                return jsonify({'message': f"A Git pull was made in the Repository: {repo_name}"}), 200
             except subprocess.CalledProcessError:
-                return jsonify({'message': f'Error al realizar git pull en el repositorio {repo_name}'}), 500
+                return jsonify({'message': f'Error while Git pull in: {repo_name}'}), 500
         else:
-            return jsonify({'message': 'No se encontró información sobre el repositorio en la carga útil (payload)'}), 400
+            return jsonify({'message': 'No information found about the repository in the load (payload)'}), 400
     else:
-        return jsonify({'message': 'La solicitud no contiene datos JSON'}), 400
+        return jsonify({'message': 'No JSON found in the request'}), 400
     
     
 if __name__ == '__main__':
